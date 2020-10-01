4415bd0958b53d015cc1f5912ee9263b55ea8a63
Andre Ments <andre@hardsearch.com.br>
2020-10-01 13:14:01 -0300

    Main view controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+/**
+ *
+ * @author suporte-01
+ */
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction() {
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction() {
+        System.out.println("onMenuItemAboutAction");
+    }
+
+    @Override
+    public void initialize(URL uri, ResourceBundle rb) {
+    }
+
+}
